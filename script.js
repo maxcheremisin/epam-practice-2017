fe7d07d16b29a9be055d2a728745f3a33bd0329c
maxcheremisin <maxcheremisin2008@gmail.com>
2017-07-01 05:44:31 +0300

    added clear lines

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-        
+
     const ctx = document.getElementById('canvas-tetris').getContext('2d');
     const COLS = 10, ROWS = 20;
     const WIDTH = 300, HEIGHT = 600;
@@ -16,9 +16,9 @@
             size: 4,
             color: 'lightblue',
             model: [0, 1, 0, 0,
-                    0, 1, 0, 0,
-                    0, 1, 0, 0,
-                    0, 1]
+                0, 1, 0, 0,
+                0, 1, 0, 0,
+                0, 1]
         },
 
         {
@@ -26,7 +26,7 @@
             size: 3,
             color: 'orange',
             model: [1, 1, 1,
-                    1]
+                1]
         },
 
         {
@@ -34,7 +34,7 @@
             size: 3,
             color: 'blue',
             model: [1, 1, 1,
-                    0, 0, 1]
+                0, 0, 1]
         },
 
         {
@@ -42,7 +42,7 @@
             size: 2,
             color: 'yellow',
             model: [1, 1,
-                    1, 1]
+                1, 1]
         },
 
         {
@@ -50,7 +50,7 @@
             size: 3,
             color: 'red',
             model: [1, 1, 0,
-                    0, 1, 1]
+                0, 1, 1]
         },
 
         {
@@ -58,7 +58,7 @@
             size: 3,
             color: 'green',
             model: [0, 1, 1,
-                    1, 1]
+                1, 1]
         },
 
         {
@@ -66,7 +66,7 @@
             size: 3,
             color: 'purple',
             model: [0, 1, 0,
-                    1, 1, 1]
+                1, 1, 1]
         }
     ];
 
@@ -84,7 +84,7 @@
                 currentShape[y][x] = shape.model[i] ? 1 : 0;
             }
         }
-        
+
         positionX = Math.floor(Math.random() * COLS);
         positionY = 0;
         for (let x = 0; x < shapeSize; x += 1) {
@@ -96,30 +96,7 @@
         }
     }
 
-    function drawShape() {
-        ctx.clearRect(0, 0, 300, 600)
-        for (let y = 0; y < shapeSize; y += 1) {
-            for (let x = 0; x < shapeSize; x += 1) {
-                if (currentShape[y][x]) {
-                    ctx.fillStyle = shapeColor;
-                    ctx.fillRect(
-                        x * WIDTH / COLS + positionX * WIDTH / COLS,
-                        y * HEIGHT / ROWS + positionY * HEIGHT / ROWS,
-                        WIDTH / COLS - 2, HEIGHT / ROWS - 2
-                        );
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 3;
-                    ctx.strokeRect(
-                        x * WIDTH / COLS + positionX * WIDTH / COLS,
-                        y * HEIGHT / ROWS + positionY * HEIGHT / ROWS,
-                        WIDTH / COLS - 1, HEIGHT / ROWS - 1
-                        );
-                }
-            }
-        }
-    }
-
-    document.body.onkeydown = function(e) {
+    document.body.onkeydown = function (e) {
         let keys = {
             37: 'left',
             39: 'right',
@@ -158,7 +135,7 @@
 
     function rotateShape(currentShape) {
         let newCurrentShape = [];
-        if (shapeSize == 4) {
+        if (shapeSize === 4) {
             for (let y = 0; y < shapeSize; y += 1) {
                 newCurrentShape[y] = [];
                 for (let x = 0; x < shapeSize; x += 1) {
@@ -188,7 +165,7 @@
                         y + offsetY >= ROWS ||
                         x + offsetX >= COLS ||
                         canvas[y + offsetY][x + offsetX]
-                        ) {
+                    ) {
                         return false;
                     }
                 }
@@ -202,6 +179,7 @@
             positionY += 1;
         } else {
             freezeShape();
+            clearLines();
             newShape();
         }
     }
@@ -212,6 +190,37 @@
                 if (currentShape[y][x]) {
                     canvas[y + positionY][x + positionX] = currentShape[y][x];
                 }
+            }
+        }
+    }
+
+    function clearLines() {
+        let newRow = [0,0,0,0,0,0,0,0,0,0];
+
+        function isFilled(block) {
+            return block === 1;
+        }
+        
+        function cutRow(y, newRow) {
+            canvas.splice(y, 1);
+            canvas.unshift(newRow);
+        }
+
+        for (let y = 0; y < ROWS; y += 1) {
+            if (canvas[y].every(isFilled)) {
+                for (let x = 0; x < COLS; x += 1) {
+                    setTimeout(function () {
+                        if (canvas[y][x]) {
+                            ctx.fillStyle = 'yellow';
+                            ctx.fillRect(
+                                x * WIDTH / COLS,
+                                y * HEIGHT / ROWS,
+                                WIDTH / COLS, HEIGHT / ROWS
+                            );
+                        }
+                    }, 10)
+                }
+                setTimeout(cutRow, 20, y, newRow);
             }
         }
     }
@@ -232,19 +241,19 @@
         for (let x = 0; x < COLS; x += 1) {
             for (let y = 0; y < ROWS; y += 1) {
                 if (canvas[y][x]) {
-                    ctx.fillStyle = "lightgreen";
+                    ctx.fillStyle = 'yellowgreen';
                     ctx.fillRect(
                         x * WIDTH / COLS,
                         y * HEIGHT / ROWS,
-                        WIDTH / COLS - 2, HEIGHT / ROWS - 2
-                                 );
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 3;
+                        WIDTH / COLS, HEIGHT / ROWS
+                    );
+                    ctx.strokeStyle = 'black';
+                    ctx.lineWidth = 2;
                     ctx.strokeRect(
                         x * WIDTH / COLS,
-                        y * HEIGHT  /ROWS,
-                        WIDTH / COLS - 1, HEIGHT / ROWS - 1
-                        );
+                        y * HEIGHT / ROWS,
+                        WIDTH / COLS, HEIGHT / ROWS
+                    );
                 }
             }
         }
@@ -257,15 +266,15 @@
                     ctx.fillRect(
                         x * WIDTH / COLS + positionX * WIDTH / COLS,
                         y * HEIGHT / ROWS + positionY * HEIGHT / ROWS,
-                        WIDTH / COLS - 2, HEIGHT / ROWS - 2
-                        );
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 3;
+                        WIDTH / COLS, HEIGHT / ROWS
+                    );
+                    ctx.strokeStyle = 'black';
+                    ctx.lineWidth = 2;
                     ctx.strokeRect(
                         x * WIDTH / COLS + positionX * WIDTH / COLS,
                         y * HEIGHT / ROWS + positionY * HEIGHT / ROWS,
-                        WIDTH / COLS - 1, HEIGHT / ROWS - 1
-                        );
+                        WIDTH / COLS, HEIGHT / ROWS
+                    );
                 }
             }
         }
@@ -279,5 +288,5 @@
     }
 
     newGame();
-    setInterval(renderCanvas, 10);
+    setInterval(renderCanvas, 20);
 }());
